4c0c57ac9a5260ecb6bbafb1fdabc069a698c5ee
alichka00 <101561291+alichka00@users.noreply.github.com>
2022-08-04 00:49:14 +0300

    project

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi praesentium
+      eaque ut quasi ea cupiditate, fugiat obcaecati dignissimos vitae minus in
+      recusandae, maxime minima facilis exercitationem quod! Voluptas quis iste
+      quos fuga quaerat culpa facilis rerum iusto non cupiditate? Modi,
+      veritatis corrupti sapiente molestiae commodi expedita natus aspernatur.
+      Ad, qui.
+    </div>
+  );
+};
+
+export default AboutPage;
